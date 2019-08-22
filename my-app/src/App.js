ce2d2e82f296062edb1d57ba16462fb341e9d55f
griffinstiens <griffinstiens@gmail.com>
2019-08-22 14:22:04 -0500

    initial homepage edits + install of node sass

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import HomePage from './homepage.component.jsx';
 import './App.css';
 
 function App() {
   return (
     <div>
-    Hello World
+     <HomePage />
     </div>
   );
 }
