b38f180b2d3a67d3ea908af15fa4ec670d13d135
griffinstiens <griffinstiens@gmail.com>
2019-08-24 13:33:27 -0500

    updated menu-item to have navigation to hats page

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,11 +1,18 @@
 import React from 'react';
+import { Route, Switch } from 'react-router-dom';
 import HomePage from './pages/homepage/homepage.component.jsx';
 
+const HatsPage = () => (
+  <div><h1>Hats Page</h1></div>
+);
 
 function App() {
   return (
     <div>
-     <HomePage />
+    <Switch>
+     <Route exact path='/' component={HomePage} />
+     <Route path='/hats' component={HatsPage} />
+    </Switch>
     </div>
   );
 }
