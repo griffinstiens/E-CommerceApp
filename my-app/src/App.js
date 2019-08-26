025732d2972b164b4fae9678923acb22f6161fd7
griffinstiens <griffinstiens@gmail.com>
2019-08-26 17:40:35 -0500

    added sign-in page with components - no authentication yet with firebase

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -3,6 +3,8 @@ import { Route, Switch } from 'react-router-dom';
 import HomePage from './pages/homepage/homepage.component.jsx';
 import ShopPage from './pages/shop/shop.component.jsx';
 import './App.css';
+import SignInAndSignUpPage from './pages/sign-in-sign-out/sign-in-sign-out.component.jsx';
+
 import Header from './components/header/header.component.jsx';
 
 function App() {
@@ -12,6 +14,7 @@ function App() {
     <Switch>
      <Route exact path='/' component={HomePage} />
      <Route path='/shop' component={ShopPage} />
+     <Route path='/signin' component={SignInAndSignUpPage} />
     </Switch>
     </div>
   );
