2ea45414180a05dca329b042dfb07718ee01532e
griffinstiens <griffinstiens@gmail.com>
2019-10-04 16:47:54 -0500

    Set up firestore, checking if user exists, pass google sign in auth to firestore

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -4,7 +4,7 @@ import HomePage from './pages/homepage/homepage.component.jsx';
 import ShopPage from './pages/shop/shop.component.jsx';
 import './App.css';
 import SignInAndSignUpPage from './pages/sign-in-sign-out/sign-in-sign-out.component.jsx';
-import { auth } from './firebase/firebase.utils.js';
+import { auth, createUserProfileDocument } from './firebase/firebase.utils.js';
 import Header from './components/header/header.component.jsx';
 
 class App extends React.Component {
@@ -12,7 +12,6 @@ class App extends React.Component {
     super();
 
     //storing state of user in APP so we can pass it into components that will need access to it
-
     this.state = {
       currentUser: null
     }
@@ -22,9 +21,29 @@ class App extends React.Component {
 
   componentDidMount() {
     //parameter is state of user -- opens subscription
-    this.unsubscribeFromAuth = auth.onAuthStateChanged(user => {
-      this.setState({ currentUser: user});
-      console.log(user);
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
+      if (userAuth) {
+
+        const userRef = await createUserProfileDocument(userAuth);
+
+        /*
+        We get a documentSnapshot object from our documentReference object. The documentSnapshot object allows us to check
+        if a document exists at this query using the .exists property which returns a boolean. We can also get the actual properties on
+        the object by calling the .data() method, which returns a JSON object of the document
+        */
+        userRef.onSnapshot(snapShot => {
+          this.setState(
+            {
+              currentUser: {
+                id: snapShot.id,
+                ...snapShot.data()
+              }
+            }
+          );
+          });   
+      } else {
+        this.setState({ currentUser: userAuth });
+      }
     });
   }
 
--- my-app/src/firebase/firebase.utils.js
+++ my-app/src/firebase/firebase.utils.js
@@ -12,6 +12,42 @@ const config = {
     appId: "1:429963388380:web:a229c798a9d3a878"
   };
 
+  /* 
+    With a query, Firestore returns two types of objects: references and snapshots. Of these objects, they can either be
+    Document or Collection versions.
+    Firestore will ALWAYS return us these objects
+  */
+  //asynchronous function to call API
+  export const createUserProfileDocument = async(userAuth, additionalData) => {
+    //if no userAuth object
+    if (!userAuth) return;
+
+    const userRef = firestore.doc(`users/${userAuth.uid}`);
+    const snapShot = await userRef.get();
+
+    //any CRUD methods (Create, Remove, Update, Delete) require use of documentRef objects, not snapshots
+    if (!snapShot.exists) {
+      const { displayName, email } = userAuth;
+      const createdAt = new Date(); //gives current date
+
+      try {
+        await userRef.set({
+          displayName, 
+          email,
+          createdAt,
+          ...additionalData
+        })
+      } catch (error) {
+          console.log("Error creating user: ", error.message);
+      }
+    }
+
+  return userRef;
+
+  }
+
+
+  //initializing firebase
   firebase.initializeApp(config);
 
   export const auth = firebase.auth();
