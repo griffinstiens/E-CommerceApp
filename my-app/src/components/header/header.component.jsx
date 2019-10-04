27aee20f5063225e04ac44cefabb64a2b391d28a
griffinstiens <griffinstiens@gmail.com>
2019-10-04 17:09:49 -0500

    Added sign up functionality with email/password

--- my-app/src/components/header/header.component.jsx
+++ my-app/src/components/header/header.component.jsx
@@ -13,7 +13,7 @@ const Header = ({ currentUser }) => (
       <Link className='option' to ='/shop'>SHOP</Link>
       <Link className='option' to ='/shop'>CONTACT</Link>
       { //display div if current user is true - link if false
-        currentUser ? <div className='option' onClick={() => auth.signOut()}>Sign Out</div> : <Link className='option' to='/signin'>SIGN IN</Link>
+        currentUser ? <div className='option' onClick={() => auth.signOut()}>SIGN OUT</div> : <Link className='option' to='/signin'>SIGN IN</Link>
       }
     </div>
   </div>
