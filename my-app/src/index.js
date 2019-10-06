514fa4b31e830a671ed3eca7ef60c5ba8e5c349b
griffinstiens <griffinstiens@gmail.com>
2019-10-06 12:16:32 -0500

    added redux-persist config to enable cart items to stay after page refresh

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -3,12 +3,18 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import App from './App';
-import store from './redux/store';
+import {PersistGate } from 'redux-persist/integration/react';
+import { store, persistor } from './redux/store';
+
 ReactDOM.render(
-  <Provider store={store}>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </Provider>,
+
+    <Provider store={store}>
+      <BrowserRouter>
+        <PersistGate persistor={persistor}>
+          <App />
+        </PersistGate>
+      </BrowserRouter>
+    </Provider>,
+
   document.getElementById('root')
 );
