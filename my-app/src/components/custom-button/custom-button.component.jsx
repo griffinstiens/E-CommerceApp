1ba77731f989f80d2e0edd0cbc060032870d2689
griffinstiens <griffinstiens@gmail.com>
2020-01-12 17:17:25 -0600

    fix buttons for now

--- my-app/src/components/custom-button/custom-button.component.jsx
+++ my-app/src/components/custom-button/custom-button.component.jsx
@@ -1,15 +1,11 @@
 import React from 'react';
-import Button from '@material-ui/core/Button';
-import ButtonGroup from '@material-ui/core/ButtonGroup';
+
 import './custom-button.styles.scss';
 
 const CustomButton = ({ children, isGoogleSignIn, inverted, ...otherProps }) => (
-  <ButtonGroup size="large" aria-label="medium outlined button group">
-    <Button>
-      {children}
-    </Button>
-    
-  </ButtonGroup>
+  <button className={`${inverted ? 'inverted' : ''} ${isGoogleSignIn ? 'google-sign-in' : ''} custom-button`} {...otherProps}>
+    {children}
+  </button>
   
 );
 
