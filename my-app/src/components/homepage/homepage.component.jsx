318f4f81eae037ebb066f9c7a9a0f24d1bd7cd3e
Griffin Stiens <griffinstiens@Griffins-MacBook-Pro.local>
2020-09-10 14:32:20 -0500

    initial UI

---
+++ my-app/src/components/homepage/homepage.component.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './homepage.styles.scss';
+
+import Directory from '../directory/directory.component';
+
+const HomePage = () => (
+    <div className='homepage'>
+       <Directory />
+    </div>
+)
+
+export default HomePage;
